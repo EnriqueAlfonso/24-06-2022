0673f321ffe34d8e8b9b5b218fcf20c7cfe79b5c
Enrique Alfonso <enrique.alfonso.lema@gmail.com>
2022-06-24 11:19:04 -0300

    1.0.0

---
+++ app.js
@@ -0,0 +1,28 @@
+/* Modulos Nativos. */
+const fs = require("fs");
+const path = require("path");
+const path_public = path.resolve(__dirname, "./public");
+
+/* Modulos Instalados. */
+const express = require("express");
+const app = express();
+
+/* Modulos Personales. */
+
+/* */
+
+/* */
+
+/* 
+===================================================================================================
+Apps.
+===================================================================================================
+*/
+
+const PORT = 3000;
+
+app.listen(PORT, console.log("Port: 3000"));
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path_public));
